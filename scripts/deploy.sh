5a85e12c551e83b09a68c681c8dc0a7a5d64557d
Rob Dabin <robert.dabin@digital.homeoffice.gov.uk>
2018-01-29 15:33:07 +0000

    fix file extensions in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 kd --insecure-skip-tls-verify \
-   -f kube/deployment.yaml \
-   -f kube/service.yaml
+   -f kube/deployment.yml \
+   -f kube/service.yml
